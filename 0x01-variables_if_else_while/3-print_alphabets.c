c7e952581876c515e7780df2b4feab786fcd13b9
john-mugo <johnmwangi13115@gmail.com>
2022-10-15 05:33:51 -0700

    fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,9 @@
 
 int main(void)
 {
-	char a;
-
 	for (a = 'a'; a <= 'z'; a++)
 		putchar(a);
-	for (a = 'A'; A <= 'Z'; a++)
+	for (a = 'A'; a <= 'Z'; a++)
 		putchar(a);
 	putchar('\n');
 	return (0);
