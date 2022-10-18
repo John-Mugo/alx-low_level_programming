de6474757b8b396172f4e1fd8795e73f9af44fab
john-mugo <johnmwangi13115@gmail.com>
2022-10-18 12:46:50 -0700

    compulsory tasks

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - main
+ *
+ * @c: character to compare
+ *
+ * Return: 1 if true, 0 if false
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#inlude "main.h"
+/**
+ * print_last_digit - Prints the last digit of a number.
+ *
+ * @i: The number in question
+ *
+ * Return: Value of the last digit.
+ */
+
+int print_last_digit(int i)
+{
+	int last_digit = i % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar(last_digit 1 '0');
+
+	return (last_digit);
+}
