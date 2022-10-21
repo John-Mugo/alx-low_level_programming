08bb30626add33a6c0ceda1da2ce1e911fd70d10
john-mugo <johnmwangi13115@gmail.com>
2022-10-21 06:38:57 -0700

    fix

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - entry point
  *
@@ -12,6 +13,12 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
+/*
+**	while (i < 10)
+**	{
+**		putchar(i);
+**	}
+**/
 
 	printf("Infinite loop avoided! \\o/\n");
 
