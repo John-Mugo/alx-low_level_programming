3ab4b3732829748bfe9439500197cb3ad78ac1a7
john-mugo <johnmwangi13115@gmail.com>
2023-01-12 22:55:13 -0800

    fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,59 +1,55 @@
 #include "lists.h"
-/**
- * insert_dnodeint_at_index - function to insert node at given index
- * @h: double pointer type to head of list
- * @idx: index at which node will be added
- * @n: int type for data to be added
- * Return: node if successful, NULL if failed
- */
+#include <stdlib.h>
 
+/**
+* insert_dnodeint_at_index - inserts a node at a certain index
+* of doubly linked list
+*
+* @h: double pointer to first node in list
+* @idx: index to add new node at
+* @n: value of new node
+* Return: address to new node or NULL on failure
+*/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *node = malloc(sizeof(dlistint_t));
-	dlistint_t *temp;
-	unsigned int count = 0;
+	unsigned int i;
+	dlistint_t *before, *after, *new_node;
 
-	if (node == NULL)
+	if (!h)
 		return (NULL);
-	node->n = n;
-	node->prev = NULL;
-	node->next = NULL;
 
-	if (*h == NULL)
-	{
-		*h = node;
-		node->next = NULL;
-		return (node);
-	}
-	temp = *h;
+	before = *h;
+	after = *h;
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
 	if (idx == 0)
 	{
-		node->next = temp;
-		temp->prev = node;
-		*h = node;
-		return (node);
+		*h = new_node;
+		new_node->next = after;
+		if (after)
+			after->prev = new_node;
+		new_node->prev = NULL;
+		return (new_node);
 	}
-
-	while (count != (idx - 1))
-	{
-		temp = temp->next;
-		count++;
-		if (temp == NULL)
-		{
-			free(node);
-			return (NULL);
-		}
-	}
-	node->next = temp->next;
-	node->prev = temp;
-	if (temp->next == NULL)
-	{
-		temp->next = node;
-	}
+	if (after)
+		after = after->next;
 	else
+		return (NULL);
+	for (i = 1; after != NULL && i < idx; i++)
 	{
-		temp->next->prev = node;
-		temp->next = node;
+		before = before->next;
+		after = after->next;
 	}
-	return (node);
+	if (i == idx)
+	{
+		before->next = new_node;
+		new_node->prev = before;
+		new_node->next = after;
+		if (after)
+			after->prev = new_node;
+		return (new_node);
+	}
+	return (NULL);
 }
